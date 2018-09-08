07c354b8ba22bc62169965ed6edabfa3d712a971
zhoupeng <zhoupeng@xjgreat.com>
2018-09-08 14:25:59 +0800

    add threaded 2 independent queue test

--- tests/test2.nim
+++ tests/test2.nim
@@ -4,13 +4,14 @@ import sched
 import times
 export sched
 import options
+import locks
 # nim c --threads:on -r src/sched.nim 
 # when isMainModule:
 
 type
     MineData = object
         a: string
-var s = initScheduler[string]()
+
 var s2 = initScheduler[MineData]()
 proc print_time(a = "default") =
     echo "From print_time:", a, " ", epochTime()
@@ -18,12 +19,13 @@ proc print_time(a = "default") =
 proc print_time2(a: MineData) =
     echo "From print_time:", a.a, " ", epochTime()
 
-proc print_some_times() =
+proc print_some_times(){.thread.} =
     echo epochTime()
+    var s = initScheduler[string]()
     discard s.enter(10, 1, print_time, "a")
     discard s.enter(5, 2, print_time, "b")
 
-    var r = s.run(false)
+    var r = s.run()
     try:
         echo r.get()
         assert(false)         # This will not be reached
@@ -31,8 +33,24 @@ proc print_some_times() =
         discard
     echo epochTime()
 
-print_some_times()
+proc print_some_times2(){.thread.} =
+    echo epochTime()
+    var s = initScheduler[string]()
+    discard s.enter(10, 1, print_time, "c")
+    discard s.enter(5, 2, print_time, "d")
 
-discard s2.enter(5, 2, print_time2, MineData(a: "minedata"))
-let r = s2.run()
+    var r = s.run()
+    try:
+        echo r.get()
+        assert(false)         # This will not be reached
+    except UnpackError: # Because an exception is raised
+        discard
+    echo epochTime()
+
+var
+    thr: array[2, Thread[void]]
+
+createThread(thr[0], print_some_times)
+createThread(thr[1], print_some_times2)
+joinThreads(thr)
 echo epochTime()
